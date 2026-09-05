c010b5176f6f214c117463305c33d55add80ca61
Bluu <Francis.Nyangoma@Gmail.com>
2026-09-05 10:36:18 +0300

    Make supblots was missing

--- modules/eda_visualizer.py
+++ modules/eda_visualizer.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import logging
 import matplotlib
 matplotlib.use('Agg')
